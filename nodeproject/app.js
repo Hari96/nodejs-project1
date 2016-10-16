9afe56f0e4825c71b3f22324df1bf94f72a18c46
Bob Blake <bob@rgbassociates.co.uk>
2016-10-16 17:37:38 +0000

    More use of routing and using Mongodb

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT;
+var eventRouter = require('./src/routes/eventRoutes');
+var dbRouter = require('./src/routes/dbRoutes');
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
@@ -9,12 +11,21 @@ app.use(express.static('bower_components'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+app.use('/Events', eventRouter);
+app.use('/Db', dbRouter);
 
 app.get('/', function(req, res){
     //res.send('Aloha World!');
     res.render('index', {
         list: ['first val', '2nd val', '3rd val'],
-        nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+        nav: [
+          { Link: 'Services', Text: 'Services'}, 
+          { Link: 'Portfolio', Text: 'Portfolio'}, 
+          { Link: 'About',Text: 'About'}, 
+          { Link: 'Team', Text: 'Team'}, 
+          { Link:'Contact', Text: 'Contact'},
+          { Link: 'Events', Text: 'Events'}
+        ]
     });
 });
 
